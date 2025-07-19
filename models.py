e1b677bbb02f34b148e94823aacf09a220b35f84
Bashar Almuntaser <basharalmuntaser2@gmail.com>
2025-07-19 19:29:06 +0300

    Update models.py

--- models.py
+++ models.py
@@ -3,7 +3,7 @@
 from sqlalchemy import Column, Integer, String, TIMESTAMP, ForeignKey, DECIMAL
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql.expression import text
-from .database import Base
+from database import Base
 
 class User(Base):
     __tablename__ = "users"
